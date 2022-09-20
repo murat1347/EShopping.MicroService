48edde51439127b3c033c858334fe2f8857ca9a6
Murat Çiçek <murat1347@yandex.com>
2022-09-20 17:20:37 +0300

    4.EventBusServiceBus Added!

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/SubscriptionInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Base
+{
+    public class SubscriptionInfo
+    {
+        public SubscriptionInfo(Type handlerType)
+        {
+            HandlerType = handlerType;
+        }
+
+        public Type HandlerType { get; } 
+
+        public static SubscriptionInfo Typed(Type handlerType)
+        {
+            return new SubscriptionInfo(handlerType);
+        }
+    }
+}
